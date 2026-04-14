8051e393b9e2d7b25bb4d6ed05a980b1af9cfa58
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-14 12:47:04 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -311,7 +311,7 @@ with tab3:
      geo_df=filtered_df[filtered_df["Geography"]==selected_geo]
      segment_churn=geo_df.groupby("ValueSegment")["Exited"].mean()*100
      st.subheader(f"Segment Churn in {selected_geo}")
-     st.vega_lite_chart(segment_churn)
+     st.area_chart(segment_churn)
 
 with tab4:
      st.header("Age and Tenure Churn Comparison")
