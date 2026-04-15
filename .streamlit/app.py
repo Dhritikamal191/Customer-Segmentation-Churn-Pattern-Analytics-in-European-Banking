e7f1e15df2cd61348a84d1e582ae6705927b3a82
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-15 19:01:53 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -395,6 +395,10 @@ with tab3:
      st.subheader(f"Segment Churn in {selected_geo}")
      st.area_chart(segment_churn)
 
+     tenure_churn=geo_df.groupby("Tenure")["Exited"].mean()*100
+     st.subheader(f"Tenure Churn in {selected_geo}")
+     st.bar_chart(tenure_churn)
+    
 with tab4:
      st.header("Age and Tenure Churn Comparison")
 
