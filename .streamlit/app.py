2d5f644501e29d31ccca9e4f37fbbcd6ffcd6d45
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-16 08:46:33 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -209,9 +209,8 @@ balance_min, balance_max=st.sidebar.slider("Balance Range", float(df["Balance"].
 
 value_filter=st.sidebar.selectbox("Customer Value Segment",["All","Low Value","Medium Value","High Value"])
 
-geo_filter=st.sidebar.selectbox("Geography",["All"]+list(df["Geography"].unique()))
+geo_filter=st.sidebar.multiselect("Geography",["All"]+list(df["Geography"].unique()))
 
-selected_country=st.sidebar.multiselect("Select Geography",options=df["Geography"].unique())
 
 gender_filter=st.sidebar.selectbox("Gender",["All"]+list(df["Gender"].unique()))
 
