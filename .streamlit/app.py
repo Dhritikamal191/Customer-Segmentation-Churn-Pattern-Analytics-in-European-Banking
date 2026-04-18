e7ff61eb18759cff9143daa8c421cdc8b9cff0ac
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-18 12:39:13 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -197,8 +197,7 @@ st.divider()
 
 #Load Dataset
 df= pd.read_csv("Data/European_Bank.csv")
-df["Retained"]==0
-df["Exited"]==1
+
 df["Geography"]=df["Geography"].fillna("Unknown")
 df["ValueSegment"]=pd.cut(df["Balance"],bins=[0,50000,100000,df["Balance"].max()],labels=["Low Value","Medium Value","High Value"])
 df["EstimatedSalary"]=df["EstimatedSalary"].astype(float)
