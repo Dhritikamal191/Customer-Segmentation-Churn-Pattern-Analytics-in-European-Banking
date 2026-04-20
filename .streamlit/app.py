84720b54d09933f643b3ab6b7391923471e8d47d
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-04-20 12:58:51 +0530

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -441,7 +441,7 @@ with col5:
                 unsafe_allow_html=True)
 
 st.divider()
-tab1, tab2, tab3,tab4=st.tabs(["Distribution","Churn Rates","Churn Drivers","Engagement Analysis"])
+tab1, tab2, tab3,tab4=st.tabs(["📈 Distribution","📉 Churn Rates","📶 Churn Drivers","👥 Engagement Analysis"])
 with tab1:
      st.subheader("Overall Customer Distribution")
 
